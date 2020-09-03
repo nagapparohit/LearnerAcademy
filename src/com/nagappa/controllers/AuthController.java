45c37a777751d8f5a19266d4d87fa691bdcade7a
rohit nagappa <rnagappa35@gmail.com>
2020-09-04 00:09:58 +0530

    added request method post in login controller

--- src/com/nagappa/controllers/AuthController.java
+++ src/com/nagappa/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.nagappa.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,7 +16,7 @@ public class AuthController {
 	}
 	
 	
-	@RequestMapping("/login")
+	@RequestMapping(value="login", method=RequestMethod.POST)
 	public String isValidUser(@RequestParam("username")String username,@RequestParam("password")String password,ModelMap map) {
 		map.addAttribute("somekey","somevalue");
 		map.addAttribute("username",username);

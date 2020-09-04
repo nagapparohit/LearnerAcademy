4b248ccec9fca8f1349504984b8b7dac65e030a0
rohit nagappa <rnagappa35@gmail.com>
2020-09-04 23:15:35 +0530

    LA-1 resolve by checking isEmpty on return list returned

--- WebContent/WEB-INF/views/login.jsp
+++ WebContent/WEB-INF/views/login.jsp
@@ -7,10 +7,15 @@
 <title>Home</title>
 </head>
 <body>
-	<form class="" action="login" method="post">
-		Enter UserName <input type="text" name="username" value=""><br>
-		Enter Password <input type="password" name="password" value=""><br>
-		<input type="submit"  value="submit" />
+	<form class="login_form" action="login" method="post">
+		<input type="text" name="username" placeholder="Enter Username" ><br><br>
+		<input type="password" name="password" placeholder="Enter password"><br><br>
+		<input type="submit"  value="submit" >
+		<a href="signup"><button type="button" name="signup">Sign up</button></a> <br><br>
+		<a href="forget_password"><button type="button" name="forget_password">Forget Password</button></a>
 	</form>
+	<span>${invalidCredentials}</span>
+
+
 </body>
 </html>
--- src/com/nagappa/controllers/AuthController.java
+++ src/com/nagappa/controllers/AuthController.java
@@ -1,27 +1,68 @@
 package com.nagappa.controllers;
 
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.nagappa.dao.HibernateUtil;
+import com.nagappa.model.UserEntity;
+
 @Controller
 public class AuthController {
 
-	
+
 	@RequestMapping("/")
 	public String showHome() {
 		return "login";
 	}
-	
-	
+
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="login", method=RequestMethod.POST)
 	public String isValidUser(@RequestParam("username")String username,@RequestParam("password")String password,ModelMap map) {
-		map.addAttribute("somekey","somevalue");
-		map.addAttribute("username",username);
-		map.addAttribute("password",password);
-		return "dashboard";
+		String view="login";
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+		Session session = sessionFactory.openSession();
+		Transaction txn = session.beginTransaction();
+
+		String queryString = "from UserEntity u where u.username = :username";
+		Query query=session.createQuery(queryString);
+		List<UserEntity> resultDB = null; 
+		try {
+				resultDB = query.setParameter("username",username).getResultList();
+		} catch (Exception e) {
+				System.out.println("No record found in DB.");
+		}
+		txn.commit();
+		session.close();
+		UserEntity result = null;
+		if(!resultDB.isEmpty()) {
+			for(UserEntity u : resultDB) {
+				result = u;
+			}
+			String user = result.getUsername();
+			String pass = result.getPassword();
+			if(password.equals(pass)) {
+				view="dashboard";
+				map.addAttribute("username",user);
+				map.addAttribute("password",pass);
+			}else {
+				map.addAttribute("invalidCredentials","you have entered invalid credentials");
+			}
+		}else {
+			map.addAttribute("invalidCredentials","Username doesnot exists");
+		}
+		return view;
 	}
-	
+
 }

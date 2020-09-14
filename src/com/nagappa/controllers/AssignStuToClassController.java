e8a0b1b187813d70209d1627489603fbaf889d96
rohit nagappa <rnagappa35@gmail.com>
2020-09-14 22:35:59 +0530

    add option to student and class selection tag dynmiaclly

--- src/com/nagappa/controllers/AssignStuToClassController.java
+++ src/com/nagappa/controllers/AssignStuToClassController.java
@@ -7,7 +7,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.nagappa.dao.ClassEntityDaoImpl;
+import com.nagappa.dao.StudentEntityDaoImpl;
 import com.nagappa.model.ClassEntity;
+import com.nagappa.model.StudentEntity;
 
 @Controller
 public class AssignStuToClassController {
@@ -18,8 +20,33 @@ public class AssignStuToClassController {
 		List<ClassEntity> listClasses = classDao.getAllClasses();
 		classDao.closeClassEntityDaoImplSession();
 		
+		StudentEntityDaoImpl studentDao = new StudentEntityDaoImpl();
+		List<StudentEntity> listStudent = studentDao.getAllStudent();
+		studentDao.closeSession();
 		
-		
+		String selectStudentOpenTag = "<select name='student' id='student'>";
+		String selectStudentCloseTag ="</select>";
+		String studentsOptionTag="";
+		for(StudentEntity student : listStudent) {
+			int admNo = student.getAddmissionNo();
+			String fname = student.getFname();
+			String lname = student.getLname();
+			studentsOptionTag += "<option value='"+admNo+"'> Admission No : "+admNo+" --> "+fname+" "+lname+"</option>";
+		}
+		String finalSelectStudentString = selectStudentOpenTag+studentsOptionTag+selectStudentCloseTag;
+		String selectClassOpenTag = "<select name='standard' id='standard'>";
+		String selectClassCloseTag = "</select>";
+		String classOptionTag="";
+		for(ClassEntity std : listClasses) {
+			char sec = std.getSection();
+			int value = std.getId();
+			String key=String.valueOf(value)+sec;
+			
+			classOptionTag += "<option value='"+key+"'>"+value+" "+sec+"</option>";
+		}
+		String finalSelectClassString = selectClassOpenTag+classOptionTag+selectClassCloseTag;
+		map.addAttribute("selectStudent", finalSelectStudentString);
+		map.addAttribute("selectClass", finalSelectClassString);
 		return "assignstudentClass";
 	}
 }
